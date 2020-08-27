bcab297372e99ce957bd6025d8e58bacc026239d
KushalShah2619 <35400198+KushalShah2619@users.noreply.github.com>
2020-08-27 21:16:28 +0530

    Updates

---
+++ precache-manifest.176a7aa15e4ce0e7d0794431cd92f5d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bccdb954e779ac43ce755f3e3f776e",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "21d873486d8f7ba15ba3",
+    "url": "/dashboard/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "36d53596e9cb9cc28080",
+    "url": "/dashboard/static/css/main.643408bd.chunk.css"
+  },
+  {
+    "revision": "21d873486d8f7ba15ba3",
+    "url": "/dashboard/static/js/2.3c027bea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dashboard/static/js/2.3c027bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d53596e9cb9cc28080",
+    "url": "/dashboard/static/js/main.9706bb02.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  },
+  {
+    "revision": "f5510067b278eb4be5bc3b973e3e6a64",
+    "url": "/dashboard/static/media/userInfoTrans.f5510067.png"
+  }
+]);
